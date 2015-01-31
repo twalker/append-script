d1fea80fc43c0ee8a378b44efcac02961795d99d
tim walker <tim.twalker@gmail.com>
2015-01-31 09:25:51 -0800

    write some actual tests

--- test/append-script.js
+++ test/append-script.js
@@ -1,10 +1,34 @@
-import {assert} from 'node_modules/chai/chai'
+import chai from 'node_modules/chai/chai'
 import appendScript from 'lib/append-script'
 
+var assert = chai.assert;
+
 describe('appendScript(url)', ()=>{
-  it('should load a script into the head', ()=>{
-    return appendScript('fixture.js')
-      .then( () => console.log('success'))
-      .catch( (e) => console.log('fail', e))
+
+  it('should load a script into the head', (done)=>{
+    appendScript('fixture.js')
+      .then( () => {
+        assert.equal(window.foo, 'bar')
+        done()
+      })
+      .catch(done)
   })
+
+  it('should load external scripts', (done)=>{
+    appendScript('http://code.jquery.com/jquery-2.1.3.min.js')
+    .then( () => {
+      assert(window.$)
+      done()
+    })
+    .catch( done)
+  })
+
+  it('should reject on network errors', (done)=>{
+    appendScript('no-exist.js')
+    .catch( (e) => {
+      assert(e)
+      done()
+    });
+  })
+
 })
--- test/fixture.js
+++ test/fixture.js
@@ -1,1 +1,1 @@
-var foo = "bar";
+window.foo = "bar";
