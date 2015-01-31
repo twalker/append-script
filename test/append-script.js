7e452ca0a780059116360c5efa9b37260e8ff9b8
tim walker <tim.twalker@gmail.com>
2015-01-30 21:48:44 -0800

    get a unit test structure

--- test/append-script.js
+++ test/append-script.js
@@ -1,9 +1,9 @@
-import {assert} from 'chai'
-import appendScript from '../dist/append-script'
+import {assert} from 'node_modules/chai/chai'
+import appendScript from 'lib/append-script'
 
 describe('appendScript(url)', ()=>{
   it('should load a script into the head', ()=>{
-    appendScript('fixture.js')
+    return appendScript('fixture.js')
       .then( () => console.log('success'))
       .catch( (e) => console.log('fail', e))
   })
